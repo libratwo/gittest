b5888b99fb34bb6cdc41471764e826efcffcd8f6
dcthink <libratwo@qq.com>
2014-06-09 11:21:15 +0800

    make other changevi main.c

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define STRLEN	64
+
 /** test git **/
 int main(int argc, char *argv[])
 {
+	int a;
+	char *b;
+
+	b = malloc(sizeof(char)*STRLEN);
+	if( b == NULL ) {
+		fprintf(stderr, "[Error] %s\n", strerror(errno));
+		return errno;
+	}
 	printf("test git!\n");
 }
