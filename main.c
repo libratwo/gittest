448e56acee4c3783cbada5acbdbcc22178b6f717
dcthink <libratwo@qq.com>
2014-06-09 11:14:41 +0800

    made a change

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
 /** test git **/
 int main(int argc, char *argv[])
 {
+	int a;
+	char b[64];
+
+	memset(b, 0x00, sizeof(b));
+
 	printf("test git!\n");
 }
